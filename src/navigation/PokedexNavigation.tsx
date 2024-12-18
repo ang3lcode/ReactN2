8db8a8681ad4d107544df45ebdaa7d295be69cf0
angel melendez <angelmelen88@gmail.com>
2024-12-18 11:46:16 -0600

    feat:Screen pokemon

--- src/navigation/PokedexNavigation.tsx
+++ src/navigation/PokedexNavigation.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { createStackNavigator } from "@react-navigation/stack";
 import PokedexScreen from '../screens/Pokedex';
+import PokemonScreen from '../screens/Pokemon';
 
 type RootStackParamList = {
 	Favorite: undefined;
 	Pokedex: undefined;
 	Account: undefined;
+	Pokemon: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -16,6 +18,7 @@ export default function PokedexNavigation() {
 			headerTitleAlign: "center", // Centra el título en todas las pantallas
 		}}>
 			<Stack.Screen name="Pokedex" component={PokedexScreen} options={{ title: "Pokedex" }} />
+			<Stack.Screen name="Pokemon" component={PokemonScreen} />
 		</Stack.Navigator>
 	)
 }
